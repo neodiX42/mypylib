7712725bb09c7cb4f41486693e9a691882345515
neodiX <neodix@ton.org>
2023-08-30 14:38:06 +0200

    get_my_work_dir

--- mypylib.py
+++ mypylib.py
@@ -327,9 +327,9 @@ class MyPyClass:
 	def get_my_work_dir(self):
 		'''return "/usr/local/bin/test/" or "/home/user/.local/share/test/"'''
 		mConfigSharedPath = "/usr/local/bin/mtc-work-dir" if platform.system() == "Darwin" else "/usr/bin/mtc-work-dir"
-		program_files_dir = self.read_file(mConfigSharedPath)
-		print("program_files_dir " + program_files_dir)
-		my_work_dir = dir(program_files_dir + self.get_my_name())
+		program_files_dir = self.read_file(mConfigSharedPath).strip()
+		my_work_dir = dir(program_files_dir + "/.local/share/" + self.get_my_name())
+		print("my_work_dir " + my_work_dir)
 		return my_work_dir
 	#end define
 
